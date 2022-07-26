fb900198a5efee790349426e197f339f6f74494b
Hyunjae Lee <leeh18@g.skku.edu>
2022-07-26 21:24:36 +0900

    configure ci/cd

--- src/main/java/ai/codemap/codemap/CodemapApplication.java
+++ src/main/java/ai/codemap/codemap/CodemapApplication.java
@@ -2,12 +2,16 @@ package ai.codemap.codemap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class CodemapApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CodemapApplication.class, args);
+//        SpringApplication app = new SpringApplication(CodemapApplication.class);
+//        app.addListeners(new ApplicationPidFileWriter());
+//        app.run(args);
     }
 
 }
